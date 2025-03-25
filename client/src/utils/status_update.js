33545c18a11848b3ebf6cabffaa524eb7e51e4d8
Elchonon <ekn400@gmail.com>
2025-03-24 22:32:59 -0400

    fixes status update dropdown options

---
+++ client/src/utils/status_update.js
@@ -0,0 +1,39 @@
+export const status_options = [
+    {
+        code: 25,
+        title: "25%"
+    },
+    {
+        code: 50,
+        title: "50%"
+    },
+    {
+        code: 75,
+        title: "75%"
+    },
+    {
+        code: 100,
+        title: "100%"
+    },
+    {
+        code: 200,
+        title: "Completed"
+    },
+    {
+        code: 300,
+        title: "Scheduled"
+    },
+    {
+        code: 400,
+        title: "In Progress"
+    },
+    {
+        code: 500,
+        title: "Stuck"
+    }
+]
+
+export const getReadableUpdateStatus = (status_code) => {
+    const res = status_options.find((status) => status.code === status_code)
+    return res.title
+}
