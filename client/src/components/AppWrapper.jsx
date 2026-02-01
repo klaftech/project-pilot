5b202eb03a08a2d0e69a10430d0473cb8b7442ae
Elchonon <ekn400@gmail.com>
2026-02-01 13:24:47 -0500

    add vercel analytics react

--- client/src/components/AppWrapper.jsx
+++ client/src/components/AppWrapper.jsx
@@ -1,15 +1,15 @@
 import Navbar from '@/components/Navbar'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const AppWrapper = ({ children }) => {
     return (
         <>
-            <Analytics />
             <Navbar />
                 {children}
             <div className="p-7 text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
                 Developed by Klaftech Data Systems
             </div>
+            <Analytics />
         </>
     )
 }
