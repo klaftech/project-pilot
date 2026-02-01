bf605d0f38c95eaccca93d22241ad11090e32d97
Elchonon <ekn400@gmail.com>
2026-02-01 13:17:38 -0500

    add vercel analytics

--- client/src/components/AppWrapper.jsx
+++ client/src/components/AppWrapper.jsx
@@ -1,8 +1,10 @@
 import Navbar from '@/components/Navbar'
+import { Analytics } from "@vercel/analytics/next"
 
 const AppWrapper = ({ children }) => {
     return (
         <>
+            <Analytics />
             <Navbar />
                 {children}
             <div className="p-7 text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
